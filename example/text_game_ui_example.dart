d592dba098cd25d06ac1446a7a5653e3956bdb2d
Chris Norman <chris.norman1989@outlook.com>
2024-08-18 19:21:54 +0100

    Be more clear about redrawing the screen in the example.

--- example/text_game_ui_example.dart
+++ example/text_game_ui_example.dart
@@ -5,8 +5,9 @@ import 'dart:async';
 import 'package:text_game_ui/text_game_ui.dart';
 
 Future<void> main() async {
-  final screen = GameScreen.fromLoadedOptions()..redrawScreen();
+  final screen = GameScreen.fromLoadedOptions();
   final keyHandler = SimpleKeyHandler.withDefaults(screen: screen);
+  screen.redrawScreen();
   await screen.run(keyHandler.handleKey);
   print('Goodbye.');
 }
